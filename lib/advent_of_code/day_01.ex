02fc997681cd410878d4022aa6e6824db0ee619a
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2024-12-01 12:15:44 +0100

    day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,19 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  alias AdventOfCode.Grid
+
+  def part1(args) do
+    grid = Grid.read_grid(args, " ", &String.to_integer/1)
+    list1 = Grid.get_col(grid, 0) |> Enum.sort()
+    list2 = Grid.get_col(grid, 1) |> Enum.sort()
+
+    Enum.zip(list1, list2) |> Enum.reduce(0, fn {num1, num2}, acc -> acc + abs(num1 - num2) end)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    grid = Grid.read_grid(args, " ", &String.to_integer/1)
+    list1 = Grid.get_col(grid, 0)
+    freqs = Grid.get_col(grid, 1) |> Enum.frequencies()
+
+    Enum.reduce(list1, 0, &(&2 + (freqs[&1] || 0) * &1))
   end
 end
