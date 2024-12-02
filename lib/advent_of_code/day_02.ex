a0cb062144e6a2eedd98ad0bc1e2c68fb8e55f1a
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2024-12-02 08:07:46 +0100

    day 2 (fast solution)

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,36 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  alias AdventOfCode.Grid
+
+  def part1(args) do
+    grid = Grid.read_grid(args, " ", &String.to_integer/1)
+
+    0..(grid.rows - 1)
+    |> Enum.map(fn i ->
+      row = Grid.get_row(grid, i)
+      is_safe(row)
+    end)
+    |> Enum.count(& &1)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    grid = Grid.read_grid(args, " ", &String.to_integer/1)
+
+    0..(grid.rows - 1)
+    |> Enum.map(fn i ->
+      row = Grid.get_row(grid, i)
+      len = length(row)
+      options = 0..(len - 1) |> Enum.map(&List.delete_at(row, &1))
+      Enum.any?(options, &is_safe(&1))
+    end)
+    |> Enum.count(& &1)
+  end
+
+  defp is_safe(row) do
+    ordered = row == Enum.sort(row) or row == Enum.sort(row, :desc)
+
+    windows = Enum.chunk_every(row, 2, 1, :discard)
+    small_steps = Enum.all?(windows, fn [num1, num2] -> abs(num1 - num2) in [1, 2, 3] end)
+
+    ordered and small_steps
   end
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -4,19 +4,23 @@ defmodule AdventOfCode.Day02Test do
   import AdventOfCode.Day02
 
   @input """
+  7 6 4 2 1
+  1 2 7 8 9
+  9 7 6 2 1
+  1 3 2 4 5
+  8 6 4 4 1
+  1 3 6 7 9
   """
 
-  @tag :skip
   test "part1" do
     result = part1(@input)
 
-    assert result
+    assert result == 2
   end
 
-  @tag :skip
   test "part2" do
     result = part2(@input)
 
-    assert result
+    assert result == 4
   end
 end
