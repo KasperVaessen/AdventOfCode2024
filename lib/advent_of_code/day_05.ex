3e1b730c165496dcd21c32ffd2e5ea62ea520568
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2024-12-05 08:00:31 +0100

    day 5

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,7 +1,57 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_args) do
+  def part1(args) do
+    [rules, instructions] = String.split(args, ["\n\n", "\r\r", "\r\n\r\n"], trim: true)
+
+    rules =
+      rules
+      |> String.split(["\n", "\r", "\r\n"], trim: true)
+      |> MapSet.new()
+
+    instructions =
+      instructions
+      |> String.split(["\n", "\r", "\r\n"], trim: true)
+      |> Enum.map(&String.split(&1, ",", trim: true))
+
+    instructions_sorted =
+      instructions
+      |> Enum.map(
+        &Enum.sort_by(&1, fn elem -> elem end, fn elem1, elem2 ->
+          key = "#{elem1}|#{elem2}"
+          MapSet.member?(rules, key)
+        end)
+      )
+
+    Enum.zip(instructions, instructions_sorted)
+    |> Enum.filter(fn {list1, list2} -> list1 == list2 end)
+    |> Enum.map(fn {_, list} -> Enum.at(list, div(length(list), 2)) |> String.to_integer() end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    [rules, instructions] = String.split(args, ["\n\n", "\r\r", "\r\n\r\n"], trim: true)
+
+    rules =
+      rules
+      |> String.split(["\n", "\r", "\r\n"], trim: true)
+      |> MapSet.new()
+
+    instructions =
+      instructions
+      |> String.split(["\n", "\r", "\r\n"], trim: true)
+      |> Enum.map(&String.split(&1, ",", trim: true))
+
+    instructions_sorted =
+      instructions
+      |> Enum.map(
+        &Enum.sort_by(&1, fn elem -> elem end, fn elem1, elem2 ->
+          key = "#{elem1}|#{elem2}"
+          MapSet.member?(rules, key)
+        end)
+      )
+
+    Enum.zip(instructions, instructions_sorted)
+    |> Enum.filter(fn {list1, list2} -> list1 != list2 end)
+    |> Enum.map(fn {_, list} -> Enum.at(list, div(length(list), 2)) |> String.to_integer() end)
+    |> Enum.sum()
   end
 end
