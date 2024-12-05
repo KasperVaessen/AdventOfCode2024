633c0e39b6aaf1e2e27a524a05a2fdbe70a15759
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2024-12-05 17:22:34 +0100

    refactor

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,33 +1,13 @@
 defmodule AdventOfCode.Day05 do
   def part1(args) do
-    [rules, instructions] = String.split(args, ["\n\n", "\r\r", "\r\n\r\n"], trim: true)
-
-    rules =
-      rules
-      |> String.split(["\n", "\r", "\r\n"], trim: true)
-      |> MapSet.new()
-
-    instructions =
-      instructions
-      |> String.split(["\n", "\r", "\r\n"], trim: true)
-      |> Enum.map(&String.split(&1, ",", trim: true))
-
-    instructions_sorted =
-      instructions
-      |> Enum.map(
-        &Enum.sort_by(&1, fn elem -> elem end, fn elem1, elem2 ->
-          key = "#{elem1}|#{elem2}"
-          MapSet.member?(rules, key)
-        end)
-      )
-
-    Enum.zip(instructions, instructions_sorted)
-    |> Enum.filter(fn {list1, list2} -> list1 == list2 end)
-    |> Enum.map(fn {_, list} -> Enum.at(list, div(length(list), 2)) |> String.to_integer() end)
-    |> Enum.sum()
+    solution(args, &(&1 == &2))
   end
 
   def part2(args) do
+    solution(args, &(&1 != &2))
+  end
+
+  defp solution(args, compare_func) do
     [rules, instructions] = String.split(args, ["\n\n", "\r\r", "\r\n\r\n"], trim: true)
 
     rules =
@@ -50,7 +30,7 @@ defmodule AdventOfCode.Day05 do
       )
 
     Enum.zip(instructions, instructions_sorted)
-    |> Enum.filter(fn {list1, list2} -> list1 != list2 end)
+    |> Enum.filter(fn {list1, list2} -> compare_func.(list1, list2) end)
     |> Enum.map(fn {_, list} -> Enum.at(list, div(length(list), 2)) |> String.to_integer() end)
     |> Enum.sum()
   end
