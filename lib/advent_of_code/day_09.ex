e202b17e13cb11ade2c4b33fde47217b1b3dac96
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2024-12-09 23:55:36 +0100

    day 9 part 1

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -1,7 +1,86 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  alias AdventOfCode.Grid
+
+  def part1(args) do
+    [first | list] = args |> Grid.read_grid(&String.to_integer/1) |> Grid.to_iterable()
+
+    files = Enum.take_every([first | list], 2) |> Enum.with_index()
+    empty = Enum.take_every(list, 2) |> Enum.map(&{&1, nil})
+
+    counts = merge(files, empty)
+
+    move_to_start(counts)
+    |> Enum.reject(fn {_, file} -> is_nil(file) end)
+    |> check_sum(0, 0)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    # [first | list] = args |> Grid.read_grid(&String.to_integer/1) |> Grid.to_iterable()
+
+    # files = Enum.take_every([first | list], 2) |> Enum.with_index()
+    # empty = Enum.take_every(list, 2) |> Enum.map(&{&1, nil})
+
+    # counts = merge(files, empty)
+  end
+
+  defp check_sum([], _, sum) do
+    sum
+  end
+
+  defp check_sum(counts, index, sum) do
+    [{count, file} | rest] = counts
+    new_sum = Enum.map(index..(index + count - 1), &(&1 * file)) |> Enum.sum()
+    check_sum(rest, index + count, sum + new_sum)
+  end
+
+  defp move_to_start(counts) do
+    counts = Enum.reject(counts, fn {amount, _} -> amount == 0 end)
+
+    if is_done?(counts) do
+      counts
+    else
+      {{amount, file}, counts} = List.pop_at(counts, -1)
+
+      if is_nil(file) do
+        move_to_start(counts)
+      else
+        {counts, remainder} =
+          Enum.flat_map_reduce(counts, amount, fn
+            {amount2, file2}, acc when acc == 0 or is_integer(file2) ->
+              {[{amount2, file2}], acc}
+
+            {amount2, nil}, acc when acc >= amount2 ->
+              {[{amount2, file}], acc - amount2}
+
+            {amount2, nil}, acc when acc < amount2 ->
+              {[{acc, file}, {amount2 - acc, nil}], 0}
+          end)
+
+        if remainder > 0 do
+          move_to_start(counts ++ [{remainder, file}])
+        else
+          move_to_start(counts)
+        end
+      end
+    end
+  end
+
+  defp is_done?(counts) do
+    {_non_nils, trailing_nils} = Enum.split_while(counts, fn {_, file} -> file != nil end)
+    Enum.empty?(trailing_nils) or Enum.all?(trailing_nils, fn {_, file} -> file == nil end)
+  end
+
+  defp print_test(list) do
+    Enum.reduce(list, "", fn {count, elem}, acc -> acc <> String.duplicate("#{elem}", count) end)
+    |> IO.puts()
+
+    list
+  end
+
+  def merge([], b), do: b
+  def merge(a, []), do: a
+
+  def merge([head_a | tail_a], [head_b | tail_b]) do
+    [head_a, head_b | merge(tail_a, tail_b)]
   end
 end
--- test/advent_of_code/day_09_test.exs
+++ test/advent_of_code/day_09_test.exs
@@ -4,19 +4,60 @@ defmodule AdventOfCode.Day09Test do
   import AdventOfCode.Day09
 
   @input """
+  2333133121414131402
   """
 
-  @tag :skip
+  @input2 """
+  12345
+  """
+
+  @input3 """
+  1111111111111111111111
+  """
+
+  @input4 """
+  90909
+  """
+
+  @inter """
+  000000000111111111222222222
+  """
+
   test "part1" do
     result = part1(@input)
 
-    assert result
+    assert result == 1928
   end
 
-  @tag :skip
+  test "part1_2" do
+    result = part1(@input2)
+
+    assert result == 60
+  end
+
+  test "part1_3" do
+    result = part1(@input3)
+
+    assert result == 290
+  end
+
+  test "part1_4" do
+    result = part1(@input4)
+
+    should_be =
+      Enum.zip(
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        0..26
+      )
+      |> Enum.map(fn {a, b} -> a * b end)
+      |> Enum.sum()
+
+    assert result == should_be
+  end
+
   test "part2" do
     result = part2(@input)
 
-    assert result
+    assert result == 2858
   end
 end
