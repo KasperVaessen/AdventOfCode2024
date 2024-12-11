cf70d65568ac9aaa79319505717ceabdcf15a599
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2024-12-11 11:14:51 +0100

    I dont like day 9

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -15,22 +15,62 @@ defmodule AdventOfCode.Day09 do
   end
 
   def part2(args) do
-    # [first | list] = args |> Grid.read_grid(&String.to_integer/1) |> Grid.to_iterable()
+    [first | list] = args |> Grid.read_grid(&String.to_integer/1) |> Grid.to_iterable()
 
-    # files = Enum.take_every([first | list], 2) |> Enum.with_index()
-    # empty = Enum.take_every(list, 2) |> Enum.map(&{&1, nil})
+    files = Enum.take_every([first | list], 2) |> Enum.with_index()
+    empty = Enum.take_every(list, 2) |> Enum.map(&{&1, nil})
 
-    # counts = merge(files, empty)
+    counts = merge(files, empty)
+
+    move_to_start2(counts)
+    |> check_sum(0, 0)
   end
 
-  defp check_sum([], _, sum) do
-    sum
+  defp move_to_start2(counts, current_to_move \\ nil) do
+    counts =
+      Enum.reject(counts, fn {amount, _} -> amount == 0 end)
+
+    index =
+      if current_to_move do
+        Enum.find_index(counts, fn {_, val} -> val == current_to_move end)
+      else
+        -1
+      end
+
+    {{amount, file}, counts} = List.pop_at(counts, index)
+    counts = List.insert_at(counts, index, {amount, nil})
+
+    if is_nil(file) do
+      move_to_start2(counts, current_to_move)
+    else
+      {counts2, _remainder} =
+        Enum.flat_map_reduce(counts, amount, fn
+          {amount2, file2}, acc when acc == 0 or is_integer(file2) or acc > amount2 ->
+            {[{amount2, file2}], acc}
+
+          {amount2, nil}, acc when acc <= amount2 ->
+            {[{acc, file}, {amount2 - acc, nil}], 0}
+        end)
+
+      counts2 = collapse(counts2)
+
+      if file == 0 do
+        counts2
+      else
+        move_to_start2(counts2, file - 1)
+      end
+    end
   end
 
-  defp check_sum(counts, index, sum) do
-    [{count, file} | rest] = counts
-    new_sum = Enum.map(index..(index + count - 1), &(&1 * file)) |> Enum.sum()
-    check_sum(rest, index + count, sum + new_sum)
+  defp collapse(list) do
+    Enum.reduce(list, [], fn
+      {b, y}, [{a, y} | rest] ->
+        [{a + b, y} | rest]
+
+      {a, y}, list ->
+        [{a, y} | list]
+    end)
+    |> Enum.reverse()
   end
 
   defp move_to_start(counts) do
@@ -75,6 +115,16 @@ defmodule AdventOfCode.Day09 do
     |> IO.puts()
 
     list
+  end
+
+  defp check_sum([], _, sum) do
+    sum
+  end
+
+  defp check_sum(counts, index, sum) do
+    [{count, file} | rest] = counts
+    new_sum = Enum.map(index..(index + count - 1), &(&1 * (file || 0))) |> Enum.sum()
+    check_sum(rest, index + count, sum + new_sum)
   end
 
   def merge([], b), do: b
